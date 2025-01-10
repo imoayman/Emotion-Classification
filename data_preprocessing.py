f4f5fc954f3670901b632721995b1e0ee3461c2c
Mo Ayman <119712667+imoayman@users.noreply.github.com>
2025-01-10 17:49:21 +0200

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -2,12 +2,11 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def create_data_generator():
     return ImageDataGenerator(
-        rotation_range=40,
-        zoom_range=0.2,                 
-        width_shift_range=0.3,         
-        height_shift_range=0.3,         
-        shear_range=0.2,                
-        horizontal_flip=True,         
-        brightness_range=[0.7, 1.3], 
-        rescale=1./255                
+        rotation_range=20,
+        zoom_range=0.15,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        horizontal_flip=True,
+        brightness_range=[0.8, 1.2],
+        rescale=1./255
     )
